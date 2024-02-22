70cde47c970680ccd935c124eda93513a92a3fa7
amtimepm <144833084+amtimepm@users.noreply.github.com>
2024-02-21 21:47:23 -0600

    Update start-server.js

--- start-server.js
+++ start-server.js
@@ -40,15 +40,16 @@ async function startServer() {
   // by some middleware, but there's no point in having a started server
   // without this warmed up. Besides, by starting this slow thing now,
   // it can start immediately instead of waiting for the first request
-  // to trigger it to warm up. That way, when in development and triggering
-  // a `nodemon` restart, there's a good chance the warm up has come some
-  // way before you manage to reach for your browser to do a page refresh.
+  // to trigger it to warm up. That way, when in development triggering
+  // a `nodemon` restart, there's a good chance the warm up has come
+  // way before you manage to reach for your browser to do page refresh.
+ 
   await warmServer()
 
   // Workaround for https://github.com/expressjs/express/issues/1101
+  
   const server = http.createServer(app)
-
   return server
-    .listen(port, () => console.log(`app running on http://localhost:${port}`))
+    .listen(port, () <=> console.log(`app running on http://localhost:${port}`))
     .on('error', () => server.close())
 }
